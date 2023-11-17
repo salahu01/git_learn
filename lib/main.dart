48f8b27a8d439ce8f877424577ac945fd124d0e8
safiya-safarin <safiyasafarin@gmail.com>
2023-11-17 12:56:09 +0530

    added text

--- lib/main.dart
+++ lib/main.dart
@@ -108,6 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
+            Text("hjdkjhfkljtextygnjvhjfk "),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
